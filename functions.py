90d67563a9dae2a7651000dbf1914637ab00bd19
Baelfire18 <jacastro18@uc.cl>
2022-02-08 11:03:46 -0300

    feat(reusme): add resume table

--- functions.py
+++ functions.py
@@ -18,6 +18,38 @@ def get_table_headers(raw_table_headers):
         table_headers_info += f'<th class="px-3 py-4 text-sm font-semibold uppercase"> {table_header.capitalize()} </th>'
     return table_headers_info
 
+def get_resume_table_headers(data):
+    headers = get_roles(data)
+    print(headers)
+    table_headers_info = ''
+    for table_header in headers:
+        table_headers_info += f'<th class="px-3 py-4 text-sm font-semibold uppercase"> {table_header} </th>'
+    return table_headers_info
+
+def get_resume_table_body(data, dicc):
+    headers = get_roles(data)
+    headers.pop(0)
+    table_body = ''
+    for month in range(1, 13):
+        month_data = dicc[month]
+        table_row = f'''<tr class="flex-col table-row mb-2 flex-no wrap text-white hover:bg-gray-600">
+                            <th class="p-3 py-4 text-center border border-grey-light">
+                                {datetime.strptime(str(month), '%m').strftime('%b')}
+                            </th>'''
+        for rol in headers:
+            if rol not in month_data:
+                count = 0
+            else:
+                count = month_data[rol]
+            table_row += f'''
+                    <td class="p-3 py-4 text-center border border-grey-light">
+                        {count}
+                    </td>
+                    '''
+        table_row += '</tr>'
+        table_body += table_row
+    return table_body
+
 def get_table_body(raw_table_body):
     table_body = ''
     for row in raw_table_body:
@@ -53,13 +85,18 @@ def format_phone_number(aux):
     except IndexError:
         return aux
 
-
 def get_color_to_rol(aux):
-    try:
+    if aux in ROLES:
         color = ROLES[aux]
-    except KeyError:
+    else:
         color = "purple"
     aux = f'''<span class="w-1/3 px-2 pb-1 text-sm font-semibold text-white bg-{color}-600 rounded-full"> 
                 {aux}
             </span> '''
     return aux
+
+def get_roles(data):
+    roles = list(set([ i[4] for i in data ]))
+    roles.sort()
+    roles.insert(0, 'Mes')
+    return roles
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from functions import read_data, get_table_headers, get_table_body
+from functions import read_data, get_table_headers, get_table_body, get_resume_table_headers, get_resume_table_body
 from params import STYLE
 
 app = FastAPI()
@@ -13,14 +13,17 @@ async def root():
     for i in range(1, 13):
         dicc[i] = {} 
     for linea in data:
-        try:
+        if linea[4] in dicc[int(linea[6].split('/')[1])]:
             dicc[int(linea[6].split('/')[1])][linea[4]] += 1
-        except KeyError:
+        else:
             dicc[int(linea[6].split('/')[1])][linea[4]] = 1
     print(dicc)
 
     table_headers = get_table_headers(headers)
     table_body = get_table_body(data)
+
+    aaa = get_resume_table_headers(data)
+    bbb = get_resume_table_body(data, dicc)
     
     return f'''
     <html>
@@ -34,6 +37,21 @@ async def root():
                 <h1 class="mt-0 mb-2 text-6xl leading-normal text-center font-sanserif text-gray-50">
                     Company
                 </h1>
+                <div class="flex items-center justify-center">
+                    <div class="container">
+                        <table class="table-header-group flex-row flex-no-wrap w-full my-5 overflow-hidden bg-gray-700 rounded-lg shadow-lg">
+                            <thead class="bg-gray-900">
+                                <tr class="flex-col table-row mb-2 text-white bg-teal-600 rounded-none rounded-l-lg flex-no wrap">
+                                {aaa}
+                                </tr>
+                            </thead>
+                            <tbody class="flex-1">
+                                {bbb}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <br />
                 <div class="flex items-center justify-center">
                     <div class="container">
                         <table class="table-header-group flex-row flex-no-wrap w-full my-5 overflow-hidden bg-gray-700 rounded-lg shadow-lg">
